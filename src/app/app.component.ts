3f05d431eb009ebf2ea048a204793a50c770046c
Rodrigo <rjaencobos@safareyes.es>
2024-10-24 17:06:05 +0200

    17:00 24/10/2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SaludoComponent} from './saludo/saludo.component';
+import {RelojComponent} from './reloj/reloj.component';
+import {TareasComponent} from './tareas/tareas.component';
+import {RegresivaComponent} from './regresiva/regresiva.component';
+import {ComentariosComponent} from './comentarios/comentarios.component';
+import {ProductoComponent} from './producto/producto.component';
+import {CalculadoraComponent} from './calculadora/calculadora.component';
+import {CarritoComponent} from './carrito/carrito.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SaludoComponent, RelojComponent, TareasComponent, RegresivaComponent, ComentariosComponent, ProductoComponent, CalculadoraComponent, CarritoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular1';
+
+  manejarComentario(comentario: string) {
+    console.log('Nuevo comentario recibido:', comentario);
+  }
 }
